dc31e558792f84172d14378df6544f0dee27df40
SamDing1999 <18dingk15@millshighsch.org>
2016-08-30 08:28:58 -0700

    TestCode for Jar File demo

---
+++ Testlar/src/TestCode.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sam Ding
+ *Date : August 30, 2016	
+ *TestCode program  -- used to show how to create a jar file that I turn in to the classroom.
+ */
+public class TestCode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
