deff133978884999e5856c5874cb2582f3050238
SamDing1999 <18dingk15@millshighsch.org>
2016-10-31 08:52:35 -0700

    1031

---
+++ ArrayLab3/src/ArraysLab3.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Sam Ding
+ * Start Date: 10/31/16
+ *This program contains 6 static methods
+ */
+public class ArraysLab3 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+	
+	/**
+	 * @param two arrays of integers <arr1> and <arr2>
+	 * @return Array of integers
+	 * 
+	 * This method accepts two arrays of integers arr1 and arr2 and returns an array of integers,
+	 * in which every element is the sum of the elements at that index for the arrays arr1 and
+	 * arr2. I assume array arr1 and arr2 have at least one element each and are the sam length.
+	 */
+	public static int[] sum(int[] arr1, int[] arr2){
+		
+	}
+}
